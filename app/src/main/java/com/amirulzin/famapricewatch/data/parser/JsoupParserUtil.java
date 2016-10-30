b3fbb3d75ee39339cac0a2160d7eb8ae609da98f
Amirul Zin <amirul.mdzin@gmail.com>
2016-10-30 20:46:40 +0800

    Implement tag lookup via element traversal missing from Jsoup

---
+++ app/src/main/java/com/amirulzin/famapricewatch/data/parser/JsoupParserUtil.java
@@ -0,0 +1,41 @@
+package com.amirulzin.famapricewatch.data.parser;
+
+import org.jsoup.nodes.Element;
+
+public class JsoupParserUtil {
+    /**
+     * Recursively find first parent with certain HTML tag.
+     *
+     * @param current Current element
+     * @param tag     HTML tag to find
+     * @return First parent of the given tag or null.
+     */
+    public static Element findFirstParentWithTag(final Element current, final String tag) {
+
+        final Element parent = current.parent();
+        if (parent != null) {
+            final String tagName = parent.tagName();
+            if (tagName.equalsIgnoreCase(tag)) {
+                return parent;
+            } else return findFirstParentWithTag(parent, tag);
+        } else return null;
+    }
+
+    /**
+     * Recursively find first sibling with certain HTML tag.
+     *
+     * @param current Current element
+     * @param tag     HTML tag to find
+     * @return First sibling of the given tag or null.
+     */
+    public static Element findFirstSiblingWithTag(final Element current, final String tag) {
+
+        final Element sibling = current.nextElementSibling();
+        if (sibling != null) {
+            final String tagName = sibling.tagName();
+            if (tagName.equalsIgnoreCase(tag)) {
+                return sibling;
+            } else return findFirstSiblingWithTag(sibling, tag);
+        } else return null;
+    }
+}
