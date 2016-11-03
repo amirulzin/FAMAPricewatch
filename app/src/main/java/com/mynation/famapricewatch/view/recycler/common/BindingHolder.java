51df45ab37f6355b2229f2f620d0607f42760e6a
Amirul Zin <amirul.mdzin@gmail.com>
2016-11-03 12:33:41 +0800

    Add databinding-recycler commons

---
+++ app/src/main/java/com/mynation/famapricewatch/view/recycler/common/BindingHolder.java
@@ -0,0 +1,22 @@
+package com.mynation.famapricewatch.view.recycler.common;
+
+import android.databinding.ViewDataBinding;
+import android.support.v7.widget.RecyclerView;
+
+/**
+ *
+ */
+
+public class BindingHolder<VB extends ViewDataBinding> extends RecyclerView.ViewHolder {
+
+    private final VB binding;
+
+    public BindingHolder(VB viewDataBinding) {
+        super(viewDataBinding.getRoot());
+        this.binding = viewDataBinding;
+    }
+
+    public VB getBinding() {
+        return binding;
+    }
+}
