9ae678a33e33e97401a10ed6d25a28a06be699b1
Amirul Zin <amirul.mdzin@gmail.com>
2016-11-03 12:34:40 +0800

    Added url redirect

--- app/src/main/java/com/mynation/famapricewatch/data/parser/FAMAEndpoint.java
+++ app/src/main/java/com/mynation/famapricewatch/data/parser/FAMAEndpoint.java
@@ -1,4 +1,4 @@
-package com.amirulzin.famapricewatch.data.parser;
+package com.mynation.famapricewatch.data.parser;
 
 /**
  *
@@ -6,12 +6,17 @@ package com.amirulzin.famapricewatch.data.parser;
 
 public class FAMAEndpoint {
     private static final String priceGenerationUrl = "https://sdvi2.fama.gov.my/price/direct/generate.asp?";
+    private static final String redirectChunk = "https://sdvi2.fama.gov.my/price/direct/price/";
 
     public static String getPriceGenerationUrl(PriceKey key) {
         return priceGenerationUrl.concat(key.getArgKey());
     }
 
-    enum PriceKey {
+    public static String getPreviousPriceReportUrl(String urlChunk) {
+        return redirectChunk + urlChunk;
+    }
+
+    public enum PriceKey {
         FARM("levelcd=04"),
         WHOLESALE("levelcd=01"),
         RETAIL("levelcd=03");
