e5eded9447de3dca098e249c1aef1631b417eae9
Amirul Zin <amirul.mdzin@gmail.com>
2016-11-09 04:37:08 +0800

    Remove RxJava and implement simpler cache

--- app/src/main/java/com/mynation/famapricewatch/presenter/CommodityDataPresenter.java
+++ app/src/main/java/com/mynation/famapricewatch/presenter/CommodityDataPresenter.java
@@ -6,6 +6,8 @@ import android.widget.Toast;
 import com.mynation.famapricewatch.data.StateData;
 import com.mynation.famapricewatch.data.parser.FAMAEndpoint;
 import com.mynation.famapricewatch.data.parser.FAMAPriceDataParser;
+import com.mynation.famapricewatch.presenter.local.LocalCacheState;
+import com.ncornette.cache.OkCacheControl;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -13,6 +15,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 import common.android.util.NetworkUtils;
 import okhttp3.Call;
@@ -25,16 +28,19 @@ import okhttp3.Response;
  * Data processing logic
  */
 
-public class CommodityDataPresenter implements FAMAPriceDataParser.FailureListener {
+public class CommodityDataPresenter implements FAMAPriceDataParser.FailureListener, OkCacheControl.NetworkMonitor {
 
-    private final OkHttpClient client = new OkHttpClient();
+    private static final int CACHE_STALE_IN_DAYS = 14;
+    private final OkHttpClient client = OkCacheControl.on(new OkHttpClient().newBuilder()).overrideServerCachePolicy(CACHE_STALE_IN_DAYS, TimeUnit.DAYS).forceCacheWhenOffline(CommodityDataPresenter.this).apply().build();
     private final Context appContext;
+    private final LocalCacheState localCacheState;
     private HashMap<String, StateData> stateDataHashMap = new HashMap<>(20, .95f);
     private ArrayList<String> stateIds = new ArrayList<>(20);
     private Set<Listener> mListeners = new HashSet<>(6, .95f);
 
     public CommodityDataPresenter(final Context applicationContext) {
         this.appContext = applicationContext;
+        this.localCacheState = new LocalCacheState(applicationContext);
     }
 
     public HashMap<String, StateData> getStateDatas() {
@@ -55,13 +61,14 @@ public class CommodityDataPresenter implements FAMAPriceDataParser.FailureListen
 
     private void executeRequest(String url) {
 
-        for (final Listener listener : mListeners) {
-            listener.onDataPreload();
-        }
-
-        if (!NetworkUtils.isConnected(appContext)) {
+        //When no cache data during first load
+        if (!localCacheState.isCacheReady() && !NetworkUtils.isConnected(appContext)) {
             Toast.makeText(appContext, "Not connected to internet", Toast.LENGTH_SHORT).show();
             return;
+        }
+
+        for (final Listener listener : mListeners) {
+            listener.onDataPreload();
         }
 
         Request.Builder builder = new Request.Builder().get().url(url);
@@ -78,6 +85,8 @@ public class CommodityDataPresenter implements FAMAPriceDataParser.FailureListen
 
             @Override
             public void onResponse(Call call, Response response) throws IOException {
+                localCacheState.setCacheReady();
+
                 String htmlBody = response.body().string();
                 ArrayList<StateData> datas = FAMAPriceDataParser.parse(htmlBody, CommodityDataPresenter.this);
                 stateIds.clear();
@@ -112,6 +121,11 @@ public class CommodityDataPresenter implements FAMAPriceDataParser.FailureListen
             executeRequest(previousSuccessfulDataUrl);
         else
             Toast.makeText(appContext, "No report currently served by FAMA", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public boolean isOnline() {
+        return NetworkUtils.isConnected(appContext);
     }
 
     public interface Listener {
---
+++ app/src/main/java/com/mynation/famapricewatch/presenter/local/LocalCacheState.java
@@ -0,0 +1,39 @@
+package com.mynation.famapricewatch.presenter.local;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ *
+ */
+
+public class LocalCacheState {
+    private final Context context;
+    private final String sharedPref = "shared_pref";
+    private final String cacheStateKey = "Cache-Ready";
+    private boolean init, cacheReady;
+
+    public LocalCacheState(Context context) {
+        this.context = context.getApplicationContext();
+        this.init = false;
+    }
+
+    public boolean isCacheReady() {
+        if (!init) {
+            init = true;
+            cacheReady = getSharedPreferences().getBoolean(cacheStateKey, false);
+        }
+        return cacheReady;
+    }
+
+
+    public void setCacheReady() {
+        cacheReady = true;
+        if (!init)
+            getSharedPreferences().edit().putBoolean(cacheStateKey, true).apply();
+    }
+
+    private SharedPreferences getSharedPreferences() {
+        return context.getSharedPreferences(sharedPref, Context.MODE_PRIVATE);
+    }
+}
